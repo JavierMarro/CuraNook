cf29a6093bedc2da71fcf072e0723c7a6542d158
Javier <javier.martinezrom@gmail.com>
2025-05-19 16:53:19 +0100

    TanStack Query installed, QueryClientProvider wrapping RouterProvider for useQuery availability

--- src/routes/browse.tsx
+++ src/routes/browse.tsx
@@ -1,6 +1,5 @@
 import { NavMenu } from "@/components/NavMenu";
 import { createFileRoute } from "@tanstack/react-router";
-import { Suspense } from "react";
 
 export const Route = createFileRoute("/browse")({
   component: Browse,
@@ -12,6 +11,7 @@ function Browse() {
       <div className="relative w-full flex items-center justify-center">
         <NavMenu className="top-2" />
         {/*TODO: Add fetching data*/}
+        {/* SUSPENSE is used to handle loading states for components that are lazy-loaded, this can be used to show a loading spinner or a placeholder while the component is being loaded. It's part of the loader TanStack router and TanStack Query is more useful for data fetching.
         <Suspense
           fallback={
             <div className="w-full h-full flex items-center justify-center">
@@ -19,8 +19,8 @@ function Browse() {
             </div>
           }
         >
-          {/* <ArtworkGrid /> */}
-        </Suspense>
+           <ArtworkGrid /> 
+        </Suspense> */}
       </div>
     </>
   );
